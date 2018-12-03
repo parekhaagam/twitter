3d921dc270d2eba9b18a18fac2d2d985522123cc
yshdave <ysd227@nyu.edu>
2018-12-02 21:35:28 -0500

    Added build code in start_Server and modified README

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+echo "------Building Authentication Server------"
+go build cmd/main/auth.go > authServer.log
+echo "------Building Storage Server------"
+go build cmd/main/storage.go > storageServer.log
+echo "------Building Web Server------"
+go build cmd/main/web.go > webServer.log
 echo "------Starting Authentication Server------"
 nohup go run cmd/main/auth.go > authServer.log &
 echo "------Starting Storage Server------"
