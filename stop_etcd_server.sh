04a3fa66cf7338526460a4099ffe3374efaf38a7
Manish Nagdevani <man514@nyu.edu>
2018-12-19 19:36:57 -0500

    automated etcd server stop

--- stop_etcd_server.sh
+++ stop_etcd_server.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+echo "Stopping etcd server"
+comm="$GOPATH/bin/goreman run stop"
+commarg="etcd"
+numOfServers=3
+
+until [ $numOfServers -lt 1 ]; do
+    command="$comm $commarg$numOfServers"
+    echo $command
+    nohup $command > raft_stop.log &
+    let numOfServers-=1
+done
