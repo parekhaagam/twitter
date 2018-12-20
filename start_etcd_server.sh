d8c7258d5cd8791f67bb2e0b47d4281ccf141615
Manish Nagdevani <man514@nyu.edu>
2018-12-19 19:36:40 -0500

    automated etcd server start

--- start_etcd_server.sh
+++ start_etcd_server.sh
@@ -1,15 +1,9 @@
+#!/usr/bin/env bash
+echo "Starting etcd server"
 
-#mkdir $ACCESSIBLE_PATH/data_dir
-chmod 777 $ACCESSIBLE_PATH/data_dir
- #export ETCD_DATA_DIR=$ACCESSIBLE_PATH/data_dir
-#mkdir go_path_sw
-chmod 777 $ACCESSIBLE_PATH/go_path_sw
-export GOPATH=$ACCESSIBLE_PATH/go_path_sw
-echo "goreman"
-#go get github.com/mattn/goreman
-echo "etcd"
-#go get github.com/etcd-io/etcd
-echo "starting"
 comm="$GOPATH/bin/goreman"
-commarg="-f $GOPATH/src/go.etcd.io/etcd/Procfile start"
-nohup start-stop-daemon --start --make-pidfile --pidfile abc.pid --exec $comm -- $commarg > raft.log &
+commarg="-f ./Procfile start"
+
+comm="$comm $commarg"
+echo $comm
+nohup $comm > raft.log &
